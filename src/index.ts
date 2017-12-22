e0ea0e6d78d28acf21331a5145ad648859242959
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-22 11:49:38 +1100

    Expose some key types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as SkiProviders from './providers';
 import * as SkiProviderHelpers from './ProviderHelpers';
+import { WebRequestData, WebRequestParam, Provider, ProviderCanonicalName } from './types/Types';
 
-export { SkiProviders, SkiProviderHelpers };
+export { SkiProviders, SkiProviderHelpers, WebRequestData, WebRequestParam, Provider, ProviderCanonicalName };
