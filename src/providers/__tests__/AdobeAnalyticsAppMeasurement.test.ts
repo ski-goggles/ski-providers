4279b2349cef2299af7a86668a4ea2d00678a6f1
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2018-06-06 22:21:44 +1000

    Add specs for POST request parsing

--- src/providers/__tests__/AdobeAnalyticsAppMeasurement.test.ts
+++ src/providers/__tests__/AdobeAnalyticsAppMeasurement.test.ts
@@ -1,115 +1,134 @@
 import { expect } from "chai";
 import "mocha";
 import { path } from "ramda";
-import { GetRequest } from "../../types/Types";
+import { GetRequest, PostRequest } from "../../types/Types";
 import { AdobeAnalyticsAppMeasurement } from "../AdobeAnalyticsAppMeasurement";
+import { AdobePostRequestDataBytes } from './fixtures'
 
 describe("Adobe Analytics Manager", () => {
   describe("transformer", () => {
-    describe("Title", () => {
-      describe("When the data contains 'pe' param", () => {
-        describe("When no events are populated", () => {
-          const rwrd: GetRequest = {
-            url: "http://someurl.tld",
-            requestType: "GET",
-            requestParams: { pe: "link_o" },
-          };
-          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-          it("returns the label Unknown Event", () => {
-            expect(path(["meta", "title"], transformed)).to.eql("Unknown Event");
-          });
-        });
-
-        describe("When an event is populated", () => {
-          const rwrd: GetRequest = {
-            url: "http://someurl.tld",
-            requestType: "GET",
-            requestParams: { pe: "link_o", events: "event1" },
-          };
-          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-          it("returns the correct event", () => {
-            expect(path(["meta", "title"], transformed)).to.eql("event1");
-          });
-        });
-      });
-
-      describe("When the data does not contain 'pe' param", () => {
-        const rwrd: GetRequest = {
-          url: "http://someurl.tld",
-          requestType: "GET",
-          requestParams: { pet: "link_o" },
-        };
-        const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-        it("returns the Page Load label", () => {
-          expect(path(["meta", "title"], transformed)).to.eql("Page Load");
-        });
-
-        describe("and contains events data", () => {
-          const rwrd: GetRequest = {
-            url: "http://someurl.tld",
-            requestType: "GET",
-            requestParams: { pet: "link_o", events: "event1" },
-          };
-          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-          it("returns Page Load label with events", () => {
-            expect(path(["meta", "title"], transformed)).to.eql("Page Load (event1)");
-          });
-        });
+    describe("POST Requests", () => {
+      const rwrd: PostRequest = {
+        url: "http://someurl.tld",
+        requestType: "POST",
+        requestBody: {
+          raw: [
+            { bytes: AdobePostRequestDataBytes }
+          ]
+        },
+      };
+      const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+      it("returns the label Unknown Event", () => {
+        expect(path(["meta", "title"], transformed)).to.eql("Page Load (event19:1528284525qjrci3uu,event10:1528284525qy6wdtcx,event4:1528284525qy6wdtcx,event12)");
       });
     });
 
-    describe("Evars, Props, and Lists", () => {
-      describe("When an evar/v property is present with the correct category", () => {
-        const rwrd: GetRequest = {
-          url: "http://someurl.tld",
-          requestType: "GET",
-          requestParams: { v1: "test", evar2: "test2" },
-        };
-        const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-        it("sets the label with a readble prefix - eVar", () => {
-          expect(path(["data", 0, "label"], transformed)).to.eql("eVar1");
-          expect(path(["data", 0, "category"], transformed)).to.eql("Evars, Props, and Lists");
-          expect(path(["data", 1, "label"], transformed)).to.eql("eVar2");
+    describe("GET Requests", () => {
+      describe("Title", () => {
+        describe("When the data contains 'pe' param", () => {
+          describe("When no events are populated", () => {
+            const rwrd: GetRequest = {
+              url: "http://someurl.tld",
+              requestType: "GET",
+              requestParams: { pe: "link_o" },
+            };
+            const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+            it("returns the label Unknown Event", () => {
+              expect(path(["meta", "title"], transformed)).to.eql("Unknown Event");
+            });
+          });
+
+          describe("When an event is populated", () => {
+            const rwrd: GetRequest = {
+              url: "http://someurl.tld",
+              requestType: "GET",
+              requestParams: { pe: "link_o", events: "event1" },
+            };
+            const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+            it("returns the correct event", () => {
+              expect(path(["meta", "title"], transformed)).to.eql("event1");
+            });
+          });
+        });
+
+        describe("When the data does not contain 'pe' param", () => {
+          const rwrd: GetRequest = {
+            url: "http://someurl.tld",
+            requestType: "GET",
+            requestParams: { pet: "link_o" },
+          };
+          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+          it("returns the Page Load label", () => {
+            expect(path(["meta", "title"], transformed)).to.eql("Page Load");
+          });
+
+          describe("and contains events data", () => {
+            const rwrd: GetRequest = {
+              url: "http://someurl.tld",
+              requestType: "GET",
+              requestParams: { pet: "link_o", events: "event1" },
+            };
+            const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+            it("returns Page Load label with events", () => {
+              expect(path(["meta", "title"], transformed)).to.eql("Page Load (event1)");
+            });
+          });
         });
       });
 
-      describe("When an prop/c property is present with the correct category", () => {
-        const rwrd: GetRequest = {
-          url: "http://someurl.tld",
-          requestType: "GET",
-          requestParams: { c1: "test", prop2: "test2" },
-        };
-        const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-        it("sets the label with a readble prefix - eVar", () => {
-          expect(path(["data", 0, "label"], transformed)).to.eql("Prop1");
-          expect(path(["data", 0, "category"], transformed)).to.eql("Evars, Props, and Lists");
-          expect(path(["data", 1, "label"], transformed)).to.eql("Prop2");
+      describe("Evars, Props, and Lists", () => {
+        describe("When an evar/v property is present with the correct category", () => {
+          const rwrd: GetRequest = {
+            url: "http://someurl.tld",
+            requestType: "GET",
+            requestParams: { v1: "test", evar2: "test2" },
+          };
+          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+          it("sets the label with a readble prefix - eVar", () => {
+            expect(path(["data", 0, "label"], transformed)).to.eql("eVar1");
+            expect(path(["data", 0, "category"], transformed)).to.eql("Evars, Props, and Lists");
+            expect(path(["data", 1, "label"], transformed)).to.eql("eVar2");
+          });
         });
-      });
 
-      describe("When an list/l property is present with the correct category", () => {
-        const rwrd: GetRequest = {
-          url: "http://someurl.tld",
-          requestType: "GET",
-          requestParams: { l1: "test", list2: "test2" },
-        };
-        const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-        it("sets the label with a readble prefix - list", () => {
-          expect(path(["data", 0, "label"], transformed)).to.eql("List1");
-          expect(path(["data", 0, "category"], transformed)).to.eql("Evars, Props, and Lists");
-          expect(path(["data", 1, "label"], transformed)).to.eql("List2");
+        describe("When an prop/c property is present with the correct category", () => {
+          const rwrd: GetRequest = {
+            url: "http://someurl.tld",
+            requestType: "GET",
+            requestParams: { c1: "test", prop2: "test2" },
+          };
+          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+          it("sets the label with a readble prefix - eVar", () => {
+            expect(path(["data", 0, "label"], transformed)).to.eql("Prop1");
+            expect(path(["data", 0, "category"], transformed)).to.eql("Evars, Props, and Lists");
+            expect(path(["data", 1, "label"], transformed)).to.eql("Prop2");
+          });
         });
-      });
 
-      describe("When a label is present that needs replacing", () => {
-        const rwrd: GetRequest = {
-          url: "http://someurl.tld",
-          requestType: "GET",
-          requestParams: { ns: "tests" },
-        };
-        const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
-        it("sets the correct label", () => {
-          expect(path(["data", 0, "label"], transformed)).to.eql("Visitor namespace");
+        describe("When an list/l property is present with the correct category", () => {
+          const rwrd: GetRequest = {
+            url: "http://someurl.tld",
+            requestType: "GET",
+            requestParams: { l1: "test", list2: "test2" },
+          };
+          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+          it("sets the label with a readble prefix - list", () => {
+            expect(path(["data", 0, "label"], transformed)).to.eql("List1");
+            expect(path(["data", 0, "category"], transformed)).to.eql("Evars, Props, and Lists");
+            expect(path(["data", 1, "label"], transformed)).to.eql("List2");
+          });
+        });
+
+        describe("When a label is present that needs replacing", () => {
+          const rwrd: GetRequest = {
+            url: "http://someurl.tld",
+            requestType: "GET",
+            requestParams: { ns: "tests" },
+          };
+          const transformed = AdobeAnalyticsAppMeasurement.transformer(rwrd);
+          it("sets the correct label", () => {
+            expect(path(["data", 0, "label"], transformed)).to.eql("Visitor namespace");
+          });
         });
       });
     });
--- src/providers/__tests__/snowplow.test.ts
+++ src/providers/__tests__/snowplow.test.ts
@@ -1,114 +1,129 @@
 import { expect } from "chai";
 import "mocha";
-import { map, mapObjIndexed, path, prop } from "ramda";
-import { GetRequest } from "../../types/Types";
+import { map, mapObjIndexed, path } from "ramda";
+import { GetRequest, PostRequest } from "../../types/Types";
 import { Snowplow } from "../Snowplow";
-import { snowplowFixture } from "./fixtures";
+import { SnowplowPostRequestBytes, snowplowFixture } from "./fixtures";
 
 describe("Snowplow", () => {
-  describe("Title", () => {
-    const eventTypes = {
-      pv: "Page View",
-      pp: "Page Ping",
-      tr: "Ecommerce transaction",
-      ti: "Ecommerce transaction",
-      se: "Custom Structured Event",
+  describe("POST Requests", () => {
+    const rwrd: PostRequest = {
+      url: "http://someurl.tld",
+      requestType: "POST",
+      requestBody: { raw: [{ bytes: SnowplowPostRequestBytes }] },
     };
+    const transformed = Snowplow.transformer(rwrd);
+    it(`returns the event type as Page View `, () => {
+      expect(path(["meta", "title"], transformed)).to.eql("Page View");
+      expect(path(["data", 0, "label"], transformed)).to.eql("Context Payload");
+    });
+  });
 
-    mapObjIndexed((eventTitle, eventKey) => {
-      describe(`when event type is set as ${eventKey}`, () => {
+  describe("GET Requests", () => {
+    describe("Title", () => {
+      const eventTypes = {
+        pv: "Page View",
+        pp: "Page Ping",
+        tr: "Ecommerce transaction",
+        ti: "Ecommerce transaction",
+        se: "Custom Structured Event",
+      };
+
+      mapObjIndexed((eventTitle, eventKey) => {
+        describe(`when event type is set as ${eventKey}`, () => {
+          const rwrd: GetRequest = {
+            url: "http://someurl.tld",
+            requestType: "GET",
+            requestParams: { e: eventKey },
+          };
+          const transformed = Snowplow.transformer(rwrd);
+          it(`returns the event type as ${eventTitle} `, () => {
+            expect(path(["meta", "title"], transformed)).to.eql(eventTitle);
+          });
+        });
+      }, eventTypes);
+
+      describe("When the data contains 'ue_px' param", () => {
         const rwrd: GetRequest = {
           url: "http://someurl.tld",
           requestType: "GET",
-          requestParams: { e: eventKey },
+          requestParams: { e: "ue", ue_px: snowplowFixture.ue_px },
         };
         const transformed = Snowplow.transformer(rwrd);
-        it(`returns the event type as ${eventTitle} `, () => {
-          expect(path(["meta", "title"], transformed)).to.eql(eventTitle);
+        it("returns the event_name as the Title", () => {
+          expect(path(["meta", "title"], transformed)).to.eql("property_details_carousel_click");
         });
       });
-    }, eventTypes);
 
-    describe("When the data contains 'ue_px' param", () => {
-      const rwrd: GetRequest = {
-        url: "http://someurl.tld",
-        requestType: "GET",
-        requestParams: { e: "ue", ue_px: snowplowFixture.ue_px },
-      };
-      const transformed = Snowplow.transformer(rwrd);
-      it("returns the event_name as the Title", () => {
-        expect(path(["meta", "title"], transformed)).to.eql("property_details_carousel_click");
+      describe("When the data does not contain 'ue_px' param", () => {
+        const rwrd: GetRequest = {
+          url: "http://someurl.tld",
+          requestType: "GET",
+          requestParams: { e: "pv" },
+        };
+        const transformed = Snowplow.transformer(rwrd);
+        it("returns the event_name as the Title", () => {
+          expect(path(["meta", "title"], transformed)).to.eql("Page View");
+        });
       });
     });
 
-    describe("When the data does not contain 'ue_px' param", () => {
+    describe("transformer", () => {
+      describe("JSON parsing", () => {
+        map(
+          param => {
+            describe(`When the data contains ${param} param`, () => {
+              describe("with a good payload", () => {
+                const requestParams = {};
+                requestParams[param] = snowplowFixture[param];
+                const rwrd: GetRequest = {
+                  url: "http://someurl.tld",
+                  requestType: "GET",
+                  requestParams: requestParams,
+                };
+                const transformed = Snowplow.transformer(rwrd);
+                it("Payload is decoded", () => {
+                  const payload = JSON.parse(path(["data", 0, "value"], transformed));
+                  expect(payload).to.be.an("object");
+                });
+              });
+
+              describe("with a bad payload", () => {
+                const requestParams = {};
+                requestParams[param] = "not-a-good-payload";
+                const rwrd: GetRequest = {
+                  url: "http://someurl.tld",
+                  requestType: "GET",
+                  requestParams: requestParams,
+                };
+                const transformer = () => Snowplow.transformer(rwrd);
+
+                it("Handles the JSON error gracefully", () => {
+                  expect(transformer).to.not.throw(SyntaxError);
+                });
+
+                it("returns a JSON object indicating an error message", () => {
+                  const parsed = JSON.parse(path(["data", 0, "value"], transformer()));
+                  expect(parsed).to.contain({ error: "Could not parse data" });
+                });
+              });
+            });
+          },
+          ["ue_px", "cx"],
+        );
+      });
+    });
+
+    describe("When a label is present that needs replacing", () => {
       const rwrd: GetRequest = {
         url: "http://someurl.tld",
         requestType: "GET",
-        requestParams: { e: "pv" },
+        requestParams: { cx: "test" },
       };
       const transformed = Snowplow.transformer(rwrd);
-      it("returns the event_name as the Title", () => {
-        expect(path(["meta", "title"], transformed)).to.eql("Page View");
+      it("sets the correct label", () => {
+        expect(path(["data", 0, "label"], transformed)).to.eql("Context Payload");
       });
-    });
-  });
-
-  describe("transformer", () => {
-    describe("JSON parsing", () => {
-      map(
-        param => {
-          describe(`When the data contains ${param} param`, () => {
-            describe("with a good payload", () => {
-              const requestParams = {}
-              requestParams[param] = snowplowFixture[param];
-              const rwrd: GetRequest = {
-                url: "http://someurl.tld",
-                requestType: "GET",
-                requestParams: requestParams,
-              };
-              const transformed = Snowplow.transformer(rwrd);
-              it("Payload is decoded", () => {
-                const payload = JSON.parse(path(["data", 0, "value"], transformed));
-                expect(payload).to.be.an("object");
-              });
-            });
-
-            describe("with a bad payload", () => {
-              const requestParams = {}
-              requestParams[param] = 'not-a-good-payload';
-              const rwrd: GetRequest = {
-                url: "http://someurl.tld",
-                requestType: "GET",
-                requestParams: requestParams,
-              };
-              const transformer = () => Snowplow.transformer(rwrd);
-
-              it("Handles the JSON error gracefully", () => {
-                expect(transformer).to.not.throw(SyntaxError);
-              });
-
-              it("returns a JSON object indicating an error message", () => {
-                const parsed = JSON.parse(path(["data", 0, "value"], transformer()));
-                expect(parsed).to.contain({ error: "Could not parse data" });
-              });
-            });
-          });
-        },
-        ["ue_px", "cx"],
-      );
-    });
-  });
-
-  describe("When a label is present that needs replacing", () => {
-    const rwrd: GetRequest = {
-      url: "http://someurl.tld",
-      requestType: "GET",
-      requestParams: { cx: "test" },
-    };
-    const transformed = Snowplow.transformer(rwrd);
-    it("sets the correct label", () => {
-      expect(path(["data", 0, "label"], transformed)).to.eql("Context Payload");
     });
   });
 });
